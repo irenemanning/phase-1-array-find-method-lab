7651bb75288cfa90c22a22d4b495f0aae6454157
Irene <irenemanningm@gmail.com>
2021-11-08 17:19:46 -0600

    commit tree 394e4ddbae50ca88e6fabdf2c50af49a11f84e5e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+function isWinner(element) {
+    return (element.result === "W")
+}
+function superbowlWin(record) {
+    const found = record.find(isWinner);
+    //const found = record.find(element => isWinner(element));
+    if (found) {
+        return found.year
+    } else {
+        return undefined
+    }  
+    
+}
